3eddb8e75ac76f7a7b984ae6b135df0fdd91b696
Austin Cavanagh <austin.cavanagh.cs@gmail.com>
2024-02-15 19:07:03 -0800

    Updated footer to have facebook, instagram, and tik tok icons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center">
+    <main className="flex flex-col items-center py-10">
       <div>Hello World</div>
     </main>
   );
