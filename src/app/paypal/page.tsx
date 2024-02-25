fe12c11efdccb37c933a4d7ac32569f0f5e2bc5a
Austin Cavanagh <austin.cavanagh.cs@gmail.com>
2024-02-25 02:52:12 -0800

    Added files and routes for paypal

--- src/app/paypal/page.tsx
+++ src/app/paypal/page.tsx
@@ -1,12 +1,12 @@
 import { stripeAction } from '@/actions/stripeAction';
-import CheckoutForm from './CheckoutForm';
+import PayPalCheckoutForm from './checkout/page';
 
 export default async function Stripe() {
   const clientSecret = (await stripeAction()) as string;
 
   return (
     <main className="flex w-full flex-1 items-center justify-center px-6 py-6 sm:p-6">
-      <CheckoutForm clientSecret={clientSecret} />
+      <PayPalCheckoutForm />
     </main>
   );
 }
