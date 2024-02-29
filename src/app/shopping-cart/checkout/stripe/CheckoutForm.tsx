cd1239114717b89c07b8a9ca4583d06c17efda58
Austin Cavanagh <austin.cavanagh.cs@gmail.com>
2024-02-28 21:33:28 -0800

    Added new shopping cart component

--- src/app/shopping-cart/checkout/stripe/CheckoutForm.tsx
+++ src/app/shopping-cart/checkout/stripe/CheckoutForm.tsx
@@ -138,13 +138,13 @@ export default function CheckoutForm({
           )}
         </span>
       </button>
-      <PaymentElement id="payment-element" options={paymentElementOptions} />
+      {/* <PaymentElement id="payment-element" options={paymentElementOptions} /> */}
       {/* <CardElement id="card-element" /> */}
-      <button disabled={isLoading || !stripe || !elements} id="submit">
+      {/* <button disabled={isLoading || !stripe || !elements} id="submit">
         <span id="button-text">
           {isLoading ? <div className="spinner" id="spinner"></div> : 'Pay now'}
         </span>
-      </button>
+      </button> */}
       {/* Show any error or success messages */}
       {message && <div id="payment-message">{message}</div>}
     </form>
