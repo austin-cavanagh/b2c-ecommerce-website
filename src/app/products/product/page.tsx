2cd7842a19bbeb40f2566f792db26d1afe5f0037
Austin Cavanagh <austin.cavanagh.cs@gmail.com>
2024-02-29 20:56:08 -0800

    Successfully added product images and rendered them from aws s3 bucket

---
+++ src/app/products/product/page.tsx
@@ -0,0 +1,11 @@
+export default async function Product() {
+  return (
+    <div>
+      <img
+        src="https://ecommerce-website-product-images.s3.us-west-1.amazonaws.com/board-1-1.JPG"
+        alt=""
+        className="h-40 w-40"
+      />
+    </div>
+  );
+}
--- src/data/products.ts
+++ src/data/products.ts
@@ -1,1 +1,13 @@
-const products = {};
+export const products = [
+  {
+    id: 1,
+    name: 'Product 1',
+    sortDescription: 'Short Description',
+    longDescription: 'Long Description',
+    imageUrl:
+      'https://ecommerce-website-product-images.s3.us-west-1.amazonaws.com/board-1-1.JPG',
+    price: 100,
+    dimensions: '5x5',
+    category: 'Test',
+  },
+];
