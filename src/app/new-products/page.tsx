15976418dc2b5046f40ccfc472dca75e5684e249
Austin Cavanagh <austin.cavanagh.cs@gmail.com>
2024-03-03 17:11:08 -0800

    Updated routes to use /products instead of /new-products, replacing the old route

--- src/app/new-products/page.tsx
+++
@@ -1,9 +0,0 @@
-import ProductsGrid from './ProductsGrid';
-
-export default async function Products() {
-  return (
-    <main className="flex w-full flex-1 flex-col items-center justify-center px-6 py-6 sm:p-6">
-      <ProductsGrid />
-    </main>
-  );
-}
