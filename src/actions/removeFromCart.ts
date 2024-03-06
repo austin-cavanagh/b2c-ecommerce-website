5f397b0882c87ba1fc11b04d153e78361ef43635
Austin Cavanagh <austin.cavanagh.cs@gmail.com>
2024-03-06 14:06:41 -0800

    Server action is successfully removing items from the cart when button is clicked

--- src/actions/removeFromCart.ts
+++ src/actions/removeFromCart.ts
@@ -1,9 +1,23 @@
 'use server';
+
 import 'server-only';
+import { prisma } from '@/prisma/prisma';
+import { revalidatePath } from 'next/cache';
 
 export async function removeFromCart(itemId: number) {
-  console.log(itemId);
+  try {
+    const deletedCartItem = await prisma.cartItem.delete({
+      where: {
+        id: itemId, // Use the item ID to identify the cart item to delete
+      },
+    });
+    console.log('Deleted cart item:', deletedCartItem);
 
-  // remove cart item from CartItem table
-  // update page to reflect new data
+    revalidatePath('/cart');
+  } catch (error) {
+    console.error('Error removing cart item:', error);
+    // Handle errors, such as item not found or database errors
+    // You might want to return an error message or status
+    // return { success: false, message: 'Failed to remove item from the cart.' };
+  }
 }
