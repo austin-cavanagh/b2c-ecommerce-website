85b8121d1095465b2df1c4c8a77213822ad056ba
Austin Cavanagh <austin.cavanagh.cs@gmail.com>
2024-03-09 15:37:32 -0800

    Updated home page opening title and message under it

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -76,11 +76,11 @@ export default async function Home() {
           <div className="relative mx-auto max-w-7xl px-4 sm:static sm:px-6 lg:px-8">
             <div className="sm:max-w-lg">
               <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
-                Summer styles are finally here
+                Look at our products
               </h1>
               <p className="mt-4 text-xl text-gray-500">
-                This year, our new summer collection will shelter you from the
-                harsh elements of a world that doesn't care if you live or die.
+                All products are handmade and carfted by me with one hundred
+                percent homemade materials
               </p>
             </div>
             <div>
