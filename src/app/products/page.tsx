c20da2cbd04bae04d7fb50ce3f70271f50d82c1e
Austin Cavanagh <austin.cavanagh.cs@gmail.com>
2024-04-21 22:39:05 -0700

    Imported new tailwind component for the products page, still need to factor it down to meet my needs

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,9 +1,12 @@
+import ProductsOverview from '@/components/products/ProductsOverview';
 import ProductsGrid from './ProductsGrid';
 
 export default async function Products() {
   return (
     <main className="flex w-full flex-1 flex-col items-center justify-center px-6 py-6 sm:p-6">
-      <ProductsGrid />
+      {/* <ProductsGrid /> */}
+
+      <ProductsOverview />
     </main>
   );
 }
