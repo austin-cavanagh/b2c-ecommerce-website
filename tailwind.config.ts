0f34b6d1f68f559eb40f14446ebf3c446911227c
Austin Cavanagh <austin.cavanagh.cs@gmail.com>
2024-04-23 16:26:45 -0700

    Found error in tailwind config that was breaking  the themes, moved the darkMode property outside of the theme object

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,8 +6,8 @@ const config: Config = {
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
+  darkMode: 'class',
   theme: {
-    darkMode: 'class',
     extend: {
       width: {
         '22': '5.5rem',
