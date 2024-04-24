87b927c76a310e13b15573b2d7501a9798f6aa00
Austin Cavanagh <austin.cavanagh.cs@gmail.com>
2024-04-23 18:57:58 -0700

    Updated the settings route components to have light and dark themes

--- src/app/account/settings/page.tsx
+++ src/app/account/settings/page.tsx
@@ -6,7 +6,7 @@ import { getServerSession } from 'next-auth';
 
 export default async function SettingsRoute() {
   const session = await getServerSession(authOptions);
-  const name = session?.user?.name;
+  const fullName = session?.user?.name;
   const email = session?.user?.email;
 
   const handlePersonalInformation = () => {
@@ -26,10 +26,10 @@ export default async function SettingsRoute() {
       {/* Personal Information */}
       <div className="grid max-w-7xl grid-cols-1 gap-x-8 gap-y-10 px-4 py-16 sm:px-6 md:grid-cols-3 lg:px-8">
         <div>
-          <h2 className="text-base font-semibold leading-7 text-white dark:text-red-800">
-            Personal Information
+          <h2 className="text-base font-semibold leading-7 text-gray-900 dark:text-white">
+            Personal information
           </h2>
-          <p className="mt-1 text-sm leading-6 text-gray-400 dark:text-blue-900">
+          <p className="mt-1 text-sm leading-6 text-gray-500 dark:text-gray-400">
             Use a permanent address where you can receive mail.
           </p>
         </div>
@@ -39,7 +39,7 @@ export default async function SettingsRoute() {
             <div className="col-span-full">
               <label
                 htmlFor="full-name"
-                className="block text-sm font-medium leading-6 text-white"
+                className="block text-sm font-medium leading-6 text-gray-900 dark:text-white"
               >
                 Full name
               </label>
@@ -49,8 +49,9 @@ export default async function SettingsRoute() {
                   name="full-name"
                   type="text"
                   autoComplete="name"
-                  placeholder={name || ''}
-                  className="block w-full rounded-md border-0 bg-white/5 py-1.5 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6"
+                  disabled
+                  defaultValue={fullName || ''}
+                  className="block w-full rounded-md border-0 bg-white/5 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-500 disabled:cursor-not-allowed disabled:bg-gray-50 disabled:text-gray-500 disabled:ring-gray-200 sm:text-sm sm:leading-6 dark:bg-white/5 dark:text-white dark:text-white dark:ring-white/10 dark:disabled:text-gray-400"
                 />
               </div>
             </div>
@@ -58,7 +59,7 @@ export default async function SettingsRoute() {
             <div className="col-span-full">
               <label
                 htmlFor="email"
-                className="block text-sm font-medium leading-6 text-white"
+                className="block text-sm font-medium leading-6 text-gray-900 dark:text-white"
               >
                 Email address
               </label>
@@ -68,51 +69,32 @@ export default async function SettingsRoute() {
                   name="email"
                   type="email"
                   autoComplete="email"
-                  placeholder={email || ''}
-                  className="block w-full rounded-md border-0 bg-white/5 py-1.5 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6"
-                />
-              </div>
-            </div>
-
-            <div>
-              <label
-                htmlFor="email"
-                className="block text-sm font-medium leading-6 text-gray-900"
-              >
-                Email
-              </label>
-              <div className="mt-2">
-                <input
-                  type="email"
-                  name="email"
-                  id="email"
-                  defaultValue="you@example.com"
                   disabled
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 disabled:cursor-not-allowed disabled:bg-gray-50 disabled:text-gray-500 disabled:ring-gray-200 sm:text-sm sm:leading-6"
-                  placeholder="you@example.com"
+                  defaultValue={email || ''}
+                  className="block w-full rounded-md border-0 bg-white/5 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-500 disabled:cursor-not-allowed disabled:bg-gray-50 disabled:text-gray-500 disabled:ring-gray-200 sm:text-sm sm:leading-6 dark:bg-white/5 dark:text-white dark:text-white dark:ring-white/10 dark:disabled:text-gray-400"
                 />
               </div>
             </div>
           </div>
 
-          <div className="mt-8 flex">
+          {/* <div className="mt-8 flex">
             <button
               type="submit"
               className="rounded-md bg-indigo-500 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
             >
               Save
             </button>
-          </div>
+          </div> */}
         </form>
       </div>
 
       {/* Change Password */}
       <div className="grid max-w-7xl grid-cols-1 gap-x-8 gap-y-10 px-4 py-16 sm:px-6 md:grid-cols-3 lg:px-8">
         <div>
-          <h2 className="text-base font-semibold leading-7 text-white">
+          <h2 className="text-base font-semibold leading-7 text-gray-900 dark:text-white">
             Change password
           </h2>
-          <p className="mt-1 text-sm leading-6 text-gray-400">
+          <p className="mt-1 text-sm leading-6 text-gray-500 dark:text-gray-400">
             Update your password associated with your account.
           </p>
         </div>
@@ -122,7 +104,7 @@ export default async function SettingsRoute() {
             <div className="col-span-full">
               <label
                 htmlFor="current-password"
-                className="block text-sm font-medium leading-6 text-white"
+                className="block text-sm font-medium leading-6 text-gray-900 dark:text-white"
               >
                 Current password
               </label>
@@ -132,7 +114,7 @@ export default async function SettingsRoute() {
                   name="current_password"
                   type="password"
                   autoComplete="current-password"
-                  className="block w-full rounded-md border-0 bg-white/5 py-1.5 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6"
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6 dark:bg-white/5 dark:text-white dark:text-white dark:ring-white/10 dark:focus:ring-indigo-500"
                 />
               </div>
             </div>
@@ -140,7 +122,7 @@ export default async function SettingsRoute() {
             <div className="col-span-full">
               <label
                 htmlFor="new-password"
-                className="block text-sm font-medium leading-6 text-white"
+                className="block text-sm font-medium leading-6 text-gray-900 dark:text-white"
               >
                 New password
               </label>
@@ -150,7 +132,7 @@ export default async function SettingsRoute() {
                   name="new_password"
                   type="password"
                   autoComplete="new-password"
-                  className="block w-full rounded-md border-0 bg-white/5 py-1.5 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6"
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6 dark:bg-white/5 dark:text-white dark:text-white dark:ring-white/10 dark:focus:ring-indigo-500"
                 />
               </div>
             </div>
@@ -158,7 +140,7 @@ export default async function SettingsRoute() {
             <div className="col-span-full">
               <label
                 htmlFor="confirm-password"
-                className="block text-sm font-medium leading-6 text-white"
+                className="block text-sm font-medium leading-6 text-gray-900 dark:text-white"
               >
                 Confirm password
               </label>
@@ -168,7 +150,7 @@ export default async function SettingsRoute() {
                   name="confirm_password"
                   type="password"
                   autoComplete="new-password"
-                  className="block w-full rounded-md border-0 bg-white/5 py-1.5 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6"
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6 dark:bg-white/5 dark:text-white dark:text-white dark:ring-white/10 dark:focus:ring-indigo-500"
                 />
               </div>
             </div>
@@ -188,13 +170,13 @@ export default async function SettingsRoute() {
       {/* Delete Account */}
       <div className="grid max-w-7xl grid-cols-1 gap-x-8 gap-y-10 px-4 py-16 sm:px-6 md:grid-cols-3 lg:px-8">
         <div>
-          <h2 className="text-base font-semibold leading-7 text-white">
+          <h2 className="text-base font-semibold leading-7 text-gray-900 dark:text-white">
             Delete account
           </h2>
-          <p className="mt-1 text-sm leading-6 text-gray-400">
+          <p className="mt-1 text-sm leading-6 text-gray-500 dark:text-gray-400">
             No longer want to use our service? You can delete your account here.
             This action is not reversible. All information related to this
-            account will be deleted permanently.
+            account will be deleted permanently.{' '}
           </p>
         </div>
 
