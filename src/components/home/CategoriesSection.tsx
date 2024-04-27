dcc46d33c6023abb77f58a95715b75ed98ace5a6
Austin Cavanagh <austin.cavanagh.cs@gmail.com>
2024-04-27 00:53:10 -0700

    Added a categoryImages.ts file to hold tet images as i am designing the category section on the homepage

--- src/components/home/CategoriesSection.tsx
+++ src/components/home/CategoriesSection.tsx
@@ -1,10 +1,28 @@
 'use server';
 import 'server-only';
 
+import { categoryImages } from '@/data/categoryImages';
+
 export default async function CategoriesSection() {
   return (
-    <section className="">
-      <h2></h2>
+    <section className="mx-auto max-w-5xl px-4 py-8">
+      <h2 className="mb-4 text-2xl font-bold">Categories</h2>
+      <div className="grid grid-cols-2 gap-4">
+        {categoryImages.map((category, index) => (
+          <div key={index} className="group relative">
+            <img
+              src={category.src}
+              alt={category.alt}
+              className="h-auto w-full object-cover"
+            />
+            <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-50 opacity-0 transition-opacity duration-300 group-hover:opacity-100">
+              <span className="text-xl font-semibold text-white">
+                {category.name}
+              </span>
+            </div>
+          </div>
+        ))}
+      </div>
     </section>
   );
 }
--- src/data/categoryImages.ts
+++ src/data/categoryImages.ts
@@ -1,3 +1,6 @@
+import holidaysImgage from './categories/holidays-1.jpeg';
+import seasonsImage from './categories/seasons-1.jpeg';
+
 export type CategoryImage = {
   name: string;
   link: string;
@@ -5,4 +8,47 @@ export type CategoryImage = {
   alt: string;
 };
 
-export const categoryImages = [];
+export const categoryImages = [
+  {
+    name: 'Holidays',
+    link: '',
+    src: holidaysImgage,
+    alt: '',
+  },
+  {
+    name: 'Seasons',
+    link: '',
+    src: seasonsImage,
+    alt: '',
+  },
+  // {
+  //   name: 'Family',
+  //   link: '',
+  //   src: '',
+  //   alt: '',
+  // },
+  // {
+  //   name: 'Sports',
+  //   link: '',
+  //   src: '',
+  //   alt: '',
+  // },
+  // {
+  //   name: 'Ornaments',
+  //   link: '',
+  //   src: '',
+  //   alt: '',
+  // },
+  // {
+  //   name: 'Newborn Gifts',
+  //   link: '',
+  //   src: '',
+  //   alt: '',
+  // },
+  // {
+  //   name: 'Romantic',
+  //   link: '',
+  //   src: '',
+  //   alt: '',
+  // },
+];
