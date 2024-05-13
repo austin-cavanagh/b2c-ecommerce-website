2a82b8c6281bc818cf2b09160876517ed7b67bf2
Austin Cavanagh <austin.cavanagh.cs@gmail.com>
2024-05-12 22:26:50 -0700

    Updated HeroSection.tsx to have 7 rows where each row will hold 2 product images

--- src/components/home/HeroSection.tsx
+++ src/components/home/HeroSection.tsx
@@ -1,3 +1,17 @@
+import React from 'react';
+
 export default function HeroSection() {
-  return <div>Test</div>;
+  return (
+    <section className="overflow-hidden bg-gray-200 p-8 text-center">
+      <div className="mx-auto space-x-5 whitespace-nowrap bg-red-500">
+        <div className="inline-block h-80 w-56 bg-indigo-700"></div>
+        <div className="inline-block h-80 w-56 bg-indigo-700"></div>
+        <div className="inline-block h-80 w-56 bg-indigo-700"></div>
+        <div className="inline-block h-80 w-56 bg-indigo-700"></div>
+        <div className="inline-block h-80 w-56 bg-indigo-700"></div>
+        <div className="inline-block h-80 w-56 bg-indigo-700"></div>
+        <div className="inline-block h-80 w-56 bg-indigo-700"></div>
+      </div>
+    </section>
+  );
 }
