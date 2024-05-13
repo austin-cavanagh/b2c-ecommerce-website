223f92fd2f30e95965085308a92918493f461bed
Austin Cavanagh <austin.cavanagh.cs@gmail.com>
2024-05-13 15:00:58 -0700

    Added ability to control the animation speed by column in HeroSection.tsx

--- src/components/home/HeroSection.tsx
+++ src/components/home/HeroSection.tsx
@@ -5,25 +5,29 @@ import { motion, useViewportScroll, useTransform } from 'framer-motion';
 export default function HeroSection() {
   const { scrollY } = useViewportScroll(); // This hook provides the current scroll distance in pixels
 
-  // Function to calculate dynamic Y offsets based on scroll distance
-  const calculateOffset = baseOffset => {
-    // Adjusting initial and final y positions based on scroll distance
-    return useTransform(scrollY, [0, 500], [baseOffset, 0]); // Adjust 500 to the scroll distance that completes the animation
+  // Function to calculate dynamic Y offsets based on scroll distance and speed multiplier
+  const calculateOffset = (baseOffset, speedMultiplier) => {
+    // Adjusting initial and final y positions based on scroll distance and speed multiplier
+    return useTransform(
+      scrollY,
+      [0, 500],
+      [baseOffset, baseOffset - 500 * speedMultiplier],
+    );
   };
 
   // Array of starting Y offsets for each column when the page loads
   const offsets = [0, 100, 200, 300, 200, 100, 0]; // Initial offsets for the animation
+  const speedMultipliers = [1.5, 2, 2.25, 2.25, 2.25, 2, 1.5]; // Speed multipliers for each column
 
   return (
     <section className="mb-40 mt-60 text-center">
-      {/* Remember to remove the mb here */}
       <div className="mx-auto w-screen p-12">
         <div className="flex justify-center space-x-4">
           {offsets.map((offset, index) => (
             <motion.div
               key={index}
               className="flex flex-col space-y-4"
-              style={{ y: calculateOffset(offset) }}
+              style={{ y: calculateOffset(offset, speedMultipliers[index]) }}
             >
               <div className="block h-80 w-56 rounded-3xl bg-indigo-700"></div>
               <div className="block h-80 w-56 rounded-3xl bg-indigo-700"></div>
