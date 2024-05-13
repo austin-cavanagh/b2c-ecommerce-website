ddf2d8dc7c67c526f9f5c4f5e29973a3bde43008
Austin Cavanagh <austin.cavanagh.cs@gmail.com>
2024-05-12 23:09:43 -0700

    Staggered the divs in HeroSection.tsx so it steps down to the center and then steps up again

--- src/components/home/HeroSection.tsx
+++ src/components/home/HeroSection.tsx
@@ -2,34 +2,34 @@ import React from 'react';
 
 export default function HeroSection() {
   return (
-    <section className="text-center">
+    <section className="mt-60 text-center">
       <div className="mx-auto w-screen overflow-hidden">
-        <div className="flex justify-center space-x-5">
-          <div className="flex flex-col space-y-5">
+        <div className="flex justify-center space-x-4">
+          <div className="flex flex-col space-y-4">
             <div className="block h-80 w-56 rounded-3xl bg-indigo-700"></div>
             <div className="block h-80 w-56 rounded-3xl bg-indigo-700"></div>
           </div>
-          <div className="flex flex-col space-y-5">
+          <div className="mt-32 flex flex-col space-y-4">
             <div className="block h-80 w-56 rounded-3xl bg-indigo-700"></div>
             <div className="block h-80 w-56 rounded-3xl bg-indigo-700"></div>
           </div>
-          <div className="flex flex-col space-y-5">
+          <div className="mt-64 flex flex-col space-y-4">
             <div className="block h-80 w-56 rounded-3xl bg-indigo-700"></div>
             <div className="block h-80 w-56 rounded-3xl bg-indigo-700"></div>
           </div>
-          <div className="flex flex-col space-y-5">
+          <div className="mt-96 flex flex-col space-y-4">
             <div className="block h-80 w-56 rounded-3xl bg-indigo-700"></div>
             <div className="block h-80 w-56 rounded-3xl bg-indigo-700"></div>
           </div>
-          <div className="flex flex-col space-y-5">
+          <div className="mt-64 flex flex-col space-y-4">
             <div className="block h-80 w-56 rounded-3xl bg-indigo-700"></div>
             <div className="block h-80 w-56 rounded-3xl bg-indigo-700"></div>
           </div>
-          <div className="flex flex-col space-y-5">
+          <div className="mt-32 flex flex-col space-y-4">
             <div className="block h-80 w-56 rounded-3xl bg-indigo-700"></div>
             <div className="block h-80 w-56 rounded-3xl bg-indigo-700"></div>
           </div>
-          <div className="flex flex-col space-y-5">
+          <div className="flex flex-col space-y-4">
             <div className="block h-80 w-56 rounded-3xl bg-indigo-700"></div>
             <div className="block h-80 w-56 rounded-3xl bg-indigo-700"></div>
           </div>
