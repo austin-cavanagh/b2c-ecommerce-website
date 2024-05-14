a0ebab274d554f4a0c3d4a9cce0e615ef5ab78fe
Austin Cavanagh <austin.cavanagh.cs@gmail.com>
2024-05-13 22:15:20 -0700

    Updated ProductOverview.tsx so the large image is has a border radius and maintained a square aspect ratio for the smaller images

--- src/components/product/ProductOverview.tsx
+++ src/components/product/ProductOverview.tsx
@@ -17,7 +17,7 @@ export default function ProductOverview({
 
   return (
     <div className="">
-      <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
+      <div className="mx-auto max-w-2xl lg:max-w-7xl">
         <div className="lg:grid lg:grid-cols-2 lg:items-start lg:gap-x-8">
           {/* Image gallery */}
           <Tab.Group as="div" className="flex flex-col-reverse">
@@ -27,18 +27,18 @@ export default function ProductOverview({
                 {product.imageUrls.map((image, index) => (
                   <Tab
                     key={index}
-                    className="relative flex h-24 cursor-pointer items-center justify-center rounded-md bg-white text-sm font-medium uppercase text-gray-900 hover:bg-gray-50 focus:outline-none focus:ring focus:ring-opacity-50 focus:ring-offset-4"
+                    className="aspect-h-1 aspect-w-1 relative flex cursor-pointer items-center justify-center rounded-md bg-white text-sm font-medium uppercase text-gray-900 hover:bg-gray-50 focus:outline-none focus:ring focus:ring-opacity-50 focus:ring-offset-4"
                   >
                     {({ selected }) => (
                       <>
                         <span className="sr-only">{image.alt}</span>
-                        <span className="absolute inset-0 overflow-hidden rounded-md">
+                        <span className="absolute inset-0 overflow-hidden rounded-3xl">
                           <Image src={image.src} alt="" fill />
                         </span>
                         <span
                           className={classNames(
                             selected ? 'ring-primary' : 'ring-transparent',
-                            'pointer-events-none absolute inset-0 rounded-md ring-2 ring-offset-2',
+                            'pointer-events-none absolute inset-0 rounded-3xl ring-2 ring-offset-2',
                           )}
                           aria-hidden="true"
                         />
@@ -52,7 +52,12 @@ export default function ProductOverview({
             <Tab.Panels className="aspect-h-1 aspect-w-1 w-full">
               {product.imageUrls.map((image, index) => (
                 <Tab.Panel key={index}>
-                  <Image src={image.src} alt={image.src} fill />
+                  <Image
+                    src={image.src}
+                    alt={image.src}
+                    fill
+                    className="rounded-3xl"
+                  />
                 </Tab.Panel>
               ))}
             </Tab.Panels>
@@ -84,12 +89,6 @@ export default function ProductOverview({
               <div className="mt-8">
                 <div className="flex items-center justify-between">
                   <h2 className="text-sm font-medium text-gray-900">Size</h2>
-                  {/* <a
-                    href="#"
-                    className="text-sm font-medium text-indigo-600 hover:text-indigo-500"
-                  >
-                    See sizing chart
-                  </a> */}
                 </div>
 
                 <RadioGroup
@@ -126,48 +125,6 @@ export default function ProductOverview({
                 </RadioGroup>
               </div>
 
-              {/* Color Picker */}
-              {/* <div>
-                <h3 className="text-sm text-gray-600">Color</h3>
-
-                <RadioGroup
-                  value={selectedColor}
-                  onChange={setSelectedColor}
-                  className="mt-2"
-                >
-                  <RadioGroup.Label className="sr-only">
-                    Choose a color
-                  </RadioGroup.Label>
-                  <span className="flex items-center space-x-3">
-                    {product.colors.map(color => (
-                      <RadioGroup.Option
-                        key={color.name}
-                        value={color}
-                        className={({ active, checked }) =>
-                          classNames(
-                            color.selectedColor,
-                            active && checked ? 'ring ring-offset-1' : '',
-                            !active && checked ? 'ring-2' : '',
-                            'relative -m-0.5 flex cursor-pointer items-center justify-center rounded-full p-0.5 focus:outline-none',
-                          )
-                        }
-                      >
-                        <RadioGroup.Label as="span" className="sr-only">
-                          {color.name}
-                        </RadioGroup.Label>
-                        <span
-                          aria-hidden="true"
-                          className={classNames(
-                            color.bgColor,
-                            'h-8 w-8 rounded-full border border-black border-opacity-10',
-                          )}
-                        />
-                      </RadioGroup.Option>
-                    ))}
-                  </span>
-                </RadioGroup>
-              </div> */}
-
               {/* Customizations */}
               <div className="mt-6 flex flex-col space-y-4">
                 {/* Text Input */}
@@ -186,7 +143,7 @@ export default function ProductOverview({
                             type="text"
                             name={option.label}
                             id={option.label}
-                            className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-gray-300 sm:text-sm sm:leading-6"
+                            className="block w-full rounded-lg border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-gray-300 sm:text-sm sm:leading-6"
                             placeholder={option.description}
                             aria-describedby={`${option.label}-description`}
                           />
@@ -200,37 +157,6 @@ export default function ProductOverview({
                       </div>
                     );
                   })}
-
-                {/* Error Text Input */}
-                {/* <div>
-                  <label
-                    htmlFor="email"
-                    className="block text-sm font-medium leading-6 text-gray-900"
-                  >
-                    Email
-                  </label>
-                  <div className="relative mt-2 rounded-md shadow-sm">
-                    <input
-                      type="email"
-                      name="email"
-                      id="email"
-                      className="block w-full rounded-md border-0 py-1.5 pr-10 text-red-900 ring-1 ring-inset ring-red-300 placeholder:text-red-300 focus:ring-2 focus:ring-inset focus:ring-red-500 sm:text-sm sm:leading-6"
-                      placeholder="you@example.com"
-                      defaultValue="adamwathan"
-                      aria-invalid="true"
-                      aria-describedby="email-error"
-                    />
-                    <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-3">
-                      <ExclamationCircleIcon
-                        className="h-5 w-5 text-red-500"
-                        aria-hidden="true"
-                      />
-                    </div>
-                  </div>
-                  <p className="mt-2 text-sm text-red-600" id="email-error">
-                    Not a valid email address.
-                  </p>
-                </div> */}
               </div>
 
               <div className="mt-10 flex">
@@ -240,71 +166,8 @@ export default function ProductOverview({
                 >
                   Add to cart
                 </button>
-
-                {/* <button
-                  type="button"
-                  className="ml-4 flex items-center justify-center rounded-md px-3 py-3 text-gray-400 hover:bg-gray-100 hover:text-gray-500"
-                >
-                  <HeartIcon
-                    className="h-6 w-6 flex-shrink-0"
-                    aria-hidden="true"
-                  />
-                  <span className="sr-only">Add to favorites</span>
-                </button> */}
               </div>
             </form>
-
-            <section aria-labelledby="details-heading" className="mt-12">
-              <h2 id="details-heading" className="sr-only">
-                Additional details
-              </h2>
-
-              {/* <div className="divide-y divide-gray-200 border-t">
-                {product.details.map((detail, index) => (
-                  <Disclosure as="div" key={index}>
-                    {({ open }) => (
-                      <>
-                        <h3>
-                          <Disclosure.Button className="group relative flex w-full items-center justify-between py-6 text-left">
-                            <span
-                              className={classNames(
-                                open ? 'text-indigo-600' : 'text-gray-900',
-                                'text-sm font-medium',
-                              )}
-                            >
-                              {detail.name}
-                            </span>
-                            <span className="ml-6 flex items-center">
-                              {open ? (
-                                <MinusIcon
-                                  className="block h-6 w-6 text-indigo-400 group-hover:text-indigo-500"
-                                  aria-hidden="true"
-                                />
-                              ) : (
-                                <PlusIcon
-                                  className="block h-6 w-6 text-gray-400 group-hover:text-gray-500"
-                                  aria-hidden="true"
-                                />
-                              )}
-                            </span>
-                          </Disclosure.Button>
-                        </h3>
-                        <Disclosure.Panel
-                          as="div"
-                          className="prose prose-sm pb-6"
-                        >
-                          <ul role="list">
-                            {detail.items.map(item => (
-                              <li key={item}>{item}</li>
-                            ))}
-                          </ul>
-                        </Disclosure.Panel>
-                      </>
-                    )}
-                  </Disclosure>
-                ))}
-              </div> */}
-            </section>
           </div>
         </div>
       </div>
