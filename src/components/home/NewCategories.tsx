994655784a009658c9b7275c8cd9b1c3f65654bb
Austin Cavanagh <austin.cavanagh.cs@gmail.com>
2024-05-15 01:18:59 -0700

    Added images to NewCategories.tsx and updated the structure to have nested nextjs images inside of each div

--- src/components/home/NewCategories.tsx
+++ src/components/home/NewCategories.tsx
@@ -2,20 +2,21 @@
 
 import { useTransform, useViewportScroll, motion } from 'framer-motion';
 
-import sportsImage from '../../data/categories-images/sports-1.jpeg';
-// import sportsImage from '../../data/categories-images/sports-2.jpeg';
+// import sportsImage from '../../data/categories-images/sports-1.jpeg';
+import sportsImage from '../../data/categories-images/sports-2.jpeg';
 
 import romanticImage from '../../data/categories-images/romantic-1.jpeg';
 // import romanticImage from '../../data/categories-images/romantic-2.jpeg';
 
 import familyImage from '../../data/categories-images/family-1.jpeg';
 
-// import holidaysImage from '../../data/categories-images/holidays-1.jpeg';
+import holidaysImage from '../../data/categories-images/holidays-1.jpeg';
 
-import seasonsImage from '../../data/categories-images/seasons-1.jpeg';
-// import seasonsImage from '../../data/categories-images/seasons-2.jpeg';
+// import seasonsImage from '../../data/categories-images/seasons-1.jpeg';
+import seasonsImage from '../../data/categories-images/seasons-2.jpeg';
 
 import ornamentsImage from '../../data/categories-images/ornaments.jpeg';
+
 import Image from 'next/image';
 
 export default function NewCategories() {
@@ -35,21 +36,48 @@ export default function NewCategories() {
     >
       <div className="flex items-start justify-center">
         <div className="flex flex-col items-end space-y-6">
-          {/* <div className="mr-10 h-48 w-48 rounded-[60px] bg-indigo-800"></div> */}
-          <div className="relative mr-10 h-48 w-48 overflow-hidden rounded-[60px]">
+          <div className="relative mr-10 h-48 w-48 overflow-hidden rounded-[60px] shadow-2xl">
             <Image
-              src={sportsImage}
+              src={familyImage}
               alt="Sports"
               layout="fill"
               objectFit="cover"
             />
           </div>
-          <div className="mr-8 h-40 w-40 rounded-[60px] bg-indigo-600"></div>
-          <div className="mr-12 h-52 w-52 rounded-[60px] bg-indigo-300"></div>
+          <div className="relative mr-8 h-40 w-40 overflow-hidden rounded-[60px] shadow-2xl">
+            <Image
+              src={ornamentsImage}
+              alt="Romantic"
+              layout="fill"
+              objectFit="cover"
+            />
+          </div>
+          <div className="relative mr-12 h-52 w-52 overflow-hidden rounded-[60px] shadow-2xl">
+            <Image
+              src={sportsImage}
+              alt="Family"
+              layout="fill"
+              objectFit="cover"
+            />
+          </div>
         </div>
         <div className="space-y-6">
-          <div className="ml-2 h-80 w-80 rounded-[60px] bg-indigo-400"></div>
-          <div className="h-64 w-64 rounded-[60px] bg-indigo-600"></div>
+          <div className="relative ml-2 h-80 w-80 overflow-hidden rounded-[60px] shadow-2xl">
+            <Image
+              src={seasonsImage}
+              alt="Seasons"
+              layout="fill"
+              objectFit="cover"
+            />
+          </div>
+          <div className="relative h-64 w-64 overflow-hidden rounded-[60px] shadow-2xl">
+            <Image
+              src={romanticImage}
+              alt="Ornaments"
+              layout="fill"
+              objectFit="cover"
+            />
+          </div>
         </div>
       </div>
       <h2 className="text-5xl font-semibold">Categories</h2>
