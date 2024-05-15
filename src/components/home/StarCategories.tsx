351f05bc3785b64ceeebb8494965ec72e21e63d0
Austin Cavanagh <austin.cavanagh.cs@gmail.com>
2024-05-15 01:36:31 -0700

    Updated StarCategories.tsx to have a nextjs image element inside each shape div - also imported placeholder images and displayed them in the component for development

--- src/components/home/StarCategories.tsx
+++ src/components/home/StarCategories.tsx
@@ -1,37 +1,76 @@
+import Image from 'next/image';
+import familyImage from '../../data/categories-images/family-1.jpeg';
+import sportsImage from '../../data/categories-images/sports-1.jpeg';
+import romanticImage from '../../data/categories-images/romantic-1.jpeg';
+import seasonsImage from '../../data/categories-images/seasons-1.jpeg';
+import ornamentsImage from '../../data/categories-images/ornaments.jpeg';
+
 export default function StarCategories() {
   return (
     <div className="my-40 flex items-center justify-center space-x-24">
       <h2 className="text-5xl font-semibold text-gray-900">Favorites</h2>
-
-      {/* <div className="flex items-center justify-center space-x-6">
-        <div className="space-y-6">
-          <div className="h-44 w-44 rounded-[44px] bg-indigo-600"></div>
-          <div className="h-44 w-44 rounded-[44px] bg-indigo-600"></div>
-        </div>
-        <div className="space-y-6">
-          <div className="h-44 w-44 rounded-[44px] bg-indigo-600"></div>
-          <div className="h-44 w-44 rounded-[44px] bg-indigo-600"></div>
-          <div className="h-44 w-44 rounded-[44px] bg-indigo-600"></div>
-        </div>
-        <div className="space-y-6">
-          <div className="h-44 w-44 rounded-[44px] bg-indigo-600"></div>
-          <div className="h-44 w-44 rounded-[44px] bg-indigo-600"></div>
-        </div>
-      </div> */}
-
       <div className="flex items-center justify-center space-x-6">
         <div className="space-y-6">
-          <div className="h-44 w-44 rounded-[44px] bg-indigo-600"></div>
-          <div className="h-44 w-44 rounded-[44px] bg-indigo-600"></div>
+          <div className="relative h-44 w-44 overflow-hidden rounded-[44px]">
+            <Image
+              src={sportsImage}
+              alt="Sports"
+              layout="fill"
+              objectFit="cover"
+            />
+          </div>
+          <div className="relative h-44 w-44 overflow-hidden rounded-[44px]">
+            <Image
+              src={romanticImage}
+              alt="Romantic"
+              layout="fill"
+              objectFit="cover"
+            />
+          </div>
         </div>
         <div className="space-y-6">
-          <div className="h-44 w-44 rounded-[44px] bg-indigo-600"></div>
-          <div className="h-44 w-44 rounded-[44px] bg-indigo-600"></div>
-          <div className="h-44 w-44 rounded-[44px] bg-indigo-600"></div>
+          <div className="relative h-44 w-44 overflow-hidden rounded-[44px]">
+            <Image
+              src={familyImage}
+              alt="Family"
+              layout="fill"
+              objectFit="cover"
+            />
+          </div>
+          <div className="relative h-44 w-44 overflow-hidden rounded-[44px]">
+            <Image
+              src={seasonsImage}
+              alt="Seasons"
+              layout="fill"
+              objectFit="cover"
+            />
+          </div>
+          <div className="relative h-44 w-44 overflow-hidden rounded-[44px]">
+            <Image
+              src={ornamentsImage}
+              alt="Ornaments"
+              layout="fill"
+              objectFit="cover"
+            />
+          </div>
         </div>
         <div className="space-y-6">
-          <div className="h-44 w-44 rounded-[44px] bg-indigo-600"></div>
-          <div className="h-44 w-44 rounded-[44px] bg-indigo-600"></div>
+          <div className="relative h-44 w-44 overflow-hidden rounded-[44px]">
+            <Image
+              src={familyImage}
+              alt="Family"
+              layout="fill"
+              objectFit="cover"
+            />
+          </div>
+          <div className="relative h-44 w-44 overflow-hidden rounded-[44px]">
+            <Image
+              src={sportsImage}
+              alt="Sports"
+              layout="fill"
+              objectFit="cover"
+            />
+          </div>
         </div>
       </div>
     </div>
